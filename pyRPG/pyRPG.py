de722c15e0a78aef4437c812ee2003d9946065c2
fuzzything44 <slimesftw@gmail.com>
2016-07-15 14:51:20 -0600

    Maybe this works

--- pyRPG/pyRPG.py
+++ pyRPG/pyRPG.py
@@ -1,6 +1,5 @@
 import time
 
-
 import world
 import world_object
 import invis_dmg
--- pyRPG/spells/spell.py
+++ pyRPG/spells/spell.py
@@ -1,5 +1,10 @@
 class spell:
-    """description of class"""
-    def __init__(this, mana):
-        this.amount = mana
+    """Class to hold spell data.
+Constructor: spell(manaCost, effect, icon): manaCost is how much mana it will cast the player to cast. Effect is the function taking a single paramater of the character casting it to actually cast the spell. Icon is the 3x3 ASCII icon to display when set."""
+    def __init__(this, manaCost, effect, icon):
+        this.amount = manaCost
+        this.cast = effect
+        this.image = icon
+    
+        
 
