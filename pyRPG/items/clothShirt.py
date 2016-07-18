b0ed2ae081ae049550fc4b47b21542931091090b
fuzzything44 <slimesftw@gmail.com>
2016-07-18 17:59:35 -0600

    Fix errors with items, remove portal
    Portal did pretty much nothing...

--- pyRPG/items/clothShirt.py
+++ pyRPG/items/clothShirt.py
@@ -1,6 +1,6 @@
 name = "Cloth shirt"
 type = "shirt"
-attributes{}
+attributes = {}
 def on_equip (this,player):
     player.attributes["maxHP"+ 5]
 def on_unequip (this, player):
--- pyRPG/items/okaySword.py
+++ pyRPG/items/okaySword.py
@@ -1,6 +1,6 @@
 name = "Okay sword"
 type = "weapon"
-attributes { "damage" : 2, "range": 10, "effects"}
+attributes = { "damage" : 2, "range": 10}
 def on_equip (this,player):
     pass
 def on_unequip (this, player):
