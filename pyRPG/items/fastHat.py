d708ff8fa41d2831c702c0f1958585992ea7bbb1
bojack the horse <alexpruitt29@yahoo.com>
2016-07-19 09:19:59 -0400

    fast armor set

---
+++ pyRPG/items/fastHat.py
@@ -0,0 +1,7 @@
+name ="fast hat"
+type = "hat"
+attributes = {}
+def on_equip (this,player):
+    player.attributes["move_spd"+2]
+def on_unequip (this, player):
+    player.attributes["move_spd"-2]
---
+++ pyRPG/items/fastShirt.py
@@ -0,0 +1,8 @@
+
+name ="fast shirt"
+type = "shirt"
+attributes = {}
+def on_equip (this,player):
+    player.attributes["move_spd"+4]
+def on_unequip (this, player):
+    player.attributes["move_spd"-4]
