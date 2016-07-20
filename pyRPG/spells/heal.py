3270756bab0187a7a4f97e3d7da65206b44050ba
CoryReck98 <cory.reck98@gmail.com>
2016-07-20 16:20:34 -0400

    Heal spell = all class base spell

---
+++ pyRPG/spells/heal.py
@@ -0,0 +1,12 @@
+import display
+
+def heal(player):
+    try:
+        player.attributes["HP"] += 1.5*player.attributes["magic"]
+        if player.attributes["HP"] >= player.attributes["maxHP"]:
+            player.attributes["HP"] = player.attributes["maxHP"]
+    except:
+        pass
+manaCost = 5
+icon = ["\\|/", "-+-", "/|\\"]
+color = display.WHITE
