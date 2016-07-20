988c9119ba6ca2d7bcf0bc257eb2355cc4e27db7
bojack the horse <alexpruitt29@yahoo.com>
2016-07-20 14:14:37 -0400

    Update gandolfRobe.py

--- pyRPG/items/gandolfRobe.py
+++ pyRPG/items/gandolfRobe.py
@@ -2,6 +2,8 @@ name ="Gandolf's robe"
 type = "shirt"
 attributes = {}
 def on_equip (this,player):
-    player.attributes["maxMP"]+50, ["maxHP"]+6
+    player.attributes["maxMP"]+=50 
+    player.attributes["maxHP"]+=6
 def on_unequip (this, player):
-    player.attributes["maxMP"]-50, ["maxHP"]-6
+    player.attributes["maxMP"]-=50 
+    player.attributes["maxHP"]-=6
