2e16597d386810eb9aa5766614da3ecb45dc8ed5
Mattharlsey <kelleym@students.stmaryshs.com>
2016-07-20 14:23:41 -0400

    tutboss mapstuff

--- pyRPG/tutboss.py
+++ pyRPG/tutboss.py
@@ -4,3 +4,4 @@ from objects import *
 def generate():
     world.map = [[ world.WORLD_GRASS for y in range(world.WORLD_Y)] for x in range(world.WORLD_X)]
     world.objects = []
+    world.objects.append(world_object.world_object(MiniBoss.MiniBoss_update, MiniBoss.MiniBosscollide, MiniBoss.MiniBossChar, MiniBoss.MiniBossColor, MiniBoss.MiniBoss_type, 25, 10, dict(MiniBoss.MiniBoss_attributes)))
