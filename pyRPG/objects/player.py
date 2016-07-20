5aa82b786480c70ffb80a684a06ecdc73efa7d66
fuzzything44 <slimesftw@gmail.com>
2016-07-20 12:05:50 -0600

    Clean up a few bugs

--- pyRPG/objects/player.py
+++ pyRPG/objects/player.py
@@ -137,7 +137,7 @@ player_attributes =                     \
       "pants" : item.item("Cloth Pants", "pants", world_object.no_func, world_object.no_func),       \
       "ring" : item.item("Useless ring", "ring", world_object.no_func, world_object.no_func),        \
       "consumable" : item.item("Nothing", "consumable", world_object.no_func, world_object.no_func), \
-      "mov_spd" : 20,                           \
+      "mov_spd" : 50,                           \
       "atk_spd" : 300,                            \
       "can_cast" : True,                          \
       "magic" : 5,                       \
