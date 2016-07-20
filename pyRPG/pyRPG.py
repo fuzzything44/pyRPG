48eb59b299faac09903c09cbbfbe7214bcd3ccf3
fuzzything44 <slimesftw@gmail.com>
2016-07-19 20:54:39 -0600

    Oops
    Forgot to include in merge

--- pyRPG/pyRPG.py
+++ pyRPG/pyRPG.py
@@ -27,6 +27,8 @@ world.save("test_world")
 
 world.map = [[ world.WORLD_GRASS for y in range(world.WORLD_Y)] for x in range(world.WORLD_X)]
 world.map[2][3] = world.WORLD_CHEST
+
+# Reset world objects from the creation and saving of the map previously
 world.objects = [world.player]
 world.objects.append(world_object.world_object(enemy.enemy_update, enemy.enemy_collide, enemy.enemyChar, enemy.enemyColor, enemy.enemy_type, 7, 7, enemy.enemy_attributes))
 world.objects.append(world_object.world_object(chest.chest_update, chest.chest_collide, chest.chest_char, chest.chest_col, chest.chest_type, 3, 3, chest.chest_attributes))
