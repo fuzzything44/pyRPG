58d77b4d35642e1dc5313148a301dc898f97ab7b
fuzzything44 <slimesftw@gmail.com>
2016-07-22 12:02:44 -0600

    tann pushed stuff

--- pyRPG/town.py
+++ pyRPG/town.py
@@ -6,16 +6,15 @@ def generate():
     world.map = [[ world.WORLD_GRASS for y in range(world.WORLD_Y)] for x in range(world.WORLD_X)]
     #world.objects = [world_object.world_object(portal.update, portal.collide, portal.char, portal.color, portal.type, 49,10,{"newmap": "end", "locx": 1, "locy": 10, "used" : False})]
     world.objects.append(world_object.world_object(portal.update, portal.collide, portal.char, portal.color, portal.type, 49,10,{"newmap": "stonstart", "locx": 0, "locy": 10, "used" : False}))
-    world.map[45][1] = world.WORLD_CHEST
 
     for x in range(16,25):
         world.map[x][16] = world.WORLD_WALL
-        world.map[x][20] = world.WORLD_WALL
+        world.map[x][19] = world.WORLD_WALL
     for y in range(16,20):
         world.map[16][y] = world.WORLD_WALL
         world.map[25][y] = world.WORLD_WALL
    
-    for x in range(17,24):
+    for x in range(17,25):
         for y in range(17,19):
             world.map[x][y] = [0,".", True]
         
@@ -33,4 +32,6 @@ def generate():
             world.map[x][y] = [0,".", True]
         
     world.map[45][5] = [0,".", True]
+    world.map[45][1] = world.WORLD_CHEST
+    world.objects.append(world_object.world_object(chest.chest_update, chest.chest_collide, chest.chest_char, chest.chest_col, chest.chest_type, 45, 2, chest.chest_attributes))
     
