8fae1fcda607b70a3d300337feeeef80d20a0532
fuzzything44 <slimesftw@gmail.com>
2016-07-22 13:16:41 -0600

    Fix world errors

--- pyRPG/ston2.py
+++ pyRPG/ston2.py
@@ -5,6 +5,6 @@ from objects import lock_portal
 def generate():
     world.objects = []
     world.map = [[ world.WORLD_STONE for y in range(world.WORLD_Y)] for x in range(world.WORLD_X)]
-    world.objects.append(world_object.world_object(lock_portal.update, lock_portal.collide, lock_portal.char, lock_portal.color, lock_portal.type, 49,10,{"newmap": "end", "locx": 0, "locy": 10, "used" : False})]
-    world.objects.append(world_object.world_object(lock_portal.update, lock_portal.collide, lock_portal.char, lock_portal.color, lock_portal.type, 0,10,{"newmap": "stone1, "locx": 0, "locy": 10, "used" : False})]
+    world.objects.append(world_object.world_object(lock_portal.update, lock_portal.collide, lock_portal.char, lock_portal.color, lock_portal.type, 49,10,{"newmap": "end", "locx": 2, "locy": 10, "used" : False}))
+    world.objects.append(world_object.world_object(lock_portal.update, lock_portal.collide, lock_portal.char, lock_portal.color, lock_portal.type, 0,10,{"newmap": "stone1", "locx": 0, "locy": 10, "used" : False}))
     world.objects.append(world_object.world_object(enemy.enemy_update, enemy.enemy_collide, enemy.enemyChar, enemy.enemyColor, enemy.enemy_type, 7, 7, dict(enemy.enemy_attributes)))
--- pyRPG/tutboss.py
+++ pyRPG/tutboss.py
@@ -4,6 +4,7 @@ from objects import MiniBoss
 from objects import lock_portal
 def generate():
     world.map = [[ world.WORLD_GRASS for y in range(world.WORLD_Y)] for x in range(world.WORLD_X)]
-    world.objects.append(world_object.world_object(lock_portal.update, lock_portal.collide, lock_portal.char, lock_portal.color, lock_portal.type, 49,10,{"newmap": "town", "locx": 0, "locy": 10, "used" : False})]
+    world.objects = []
+    world.objects.append(world_object.world_object(lock_portal.update, lock_portal.collide, lock_portal.char, lock_portal.color, lock_portal.type, 49,10,{"newmap": "town", "locx": 0, "locy": 10, "used" : False}))
 
     world.objects.append(world_object.world_object(MiniBoss.MiniBoss_update, MiniBoss.MiniBosscollide, MiniBoss.MiniBossChar, MiniBoss.MiniBossColor, MiniBoss.MiniBoss_type, 25, 10, dict(MiniBoss.MiniBoss_attributes)))
