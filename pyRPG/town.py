a57ff85bf5afcd9bea4a2376600c4313dca37b9c
Mattharlsey <kelleym@students.stmaryshs.com>
2016-07-22 13:55:08 -0400

    town

--- pyRPG/town.py
+++ pyRPG/town.py
@@ -5,9 +5,21 @@ def generate():
     world.objects = []
     world.map = [[ world.WORLD_GRASS for y in range(world.WORLD_Y)] for x in range(world.WORLD_X)]
     #world.objects = [world_object.world_object(portal.update, portal.collide, portal.char, portal.color, portal.type, 49,10,{"newmap": "end", "locx": 1, "locy": 10, "used" : False})]
-    world.objects.append(world_object.world_object(portal.update, portal.collide, portal.char, portal.color, portal.type, 45,10,{"newmap": "stonstart", "locx": 0, "locy": 10, "used" : False}))
+    world.objects.append(world_object.world_object(portal.update, portal.collide, portal.char, portal.color, portal.type, 49,10,{"newmap": "stonstart", "locx": 0, "locy": 10, "used" : False}))
     world.map[45][1] = world.WORLD_CHEST
 
+    for x in range(16,25):
+        world.map[x][16] = world.WORLD_WALL
+        world.map[x][20] = world.WORLD_WALL
+    for y in range(16,20):
+        world.map[16][y] = world.WORLD_WALL
+        world.map[25][y] = world.WORLD_WALL
+   
+    for x in range(17,24):
+        for y in range(17,19):
+            world.map[x][y] = [0,".", True]
+        
+    world.map[19][16] = [0,".", True]
     
     for x in range(42,49):
         world.map[x][0] = world.WORLD_WALL
