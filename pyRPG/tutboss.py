d1dfc8c4fc10a6370e951ad46ebb7e6150b68afc
Mattharlsey <kelleym@students.stmaryshs.com>
2016-07-23 09:37:17 -0400

    tutboss tips

--- pyRPG/tutboss.py
+++ pyRPG/tutboss.py
@@ -8,13 +8,21 @@ def generate():
     world.objects.append(world_object.world_object(lock_portal.update, lock_portal.collide, lock_portal.char, lock_portal.color, lock_portal.type, 49,10,{"newmap": "town", "locx": 0, "locy": 10, "used" : False}))
     world.objects.append(world_object.world_object(MiniBoss.MiniBoss_update, MiniBoss.MiniBosscollide, MiniBoss.MiniBossChar, MiniBoss.MiniBossColor, MiniBoss.MiniBoss_type, 25, 10, dict(MiniBoss.MiniBoss_attributes)))
 
-    world.map[30][15] = [1, '#', True]
+    world.map[30][0] = [6, '@', True]
+    world.map[31][0] = [0, '=', True]
+    world.map[32][0] = [0, 'C', True]
+    world.map[33][0] = [0, 'h', True]
+    world.map[34][0] = [0, 'e', True]
+    world.map[35][0] = [0, 's', True]
+    world.map[36][0] = [0, 't', True]
+
+    world.map[30][1] = [1, '#', True]
     world.objects.append(world_object.world_object(invis_dmg.update, invis_dmg.fire_collide, invis_dmg.char, invis_dmg.color, invis_dmg.type, 30, 15, {}))
-    world.map[31][15] = [0, '=', True]
-    world.map[32][15] = [0, 'L', True]
-    world.map[33][15] = [0, 'a', True]
-    world.map[34][15] = [0, 'v', True]
-    world.map[35][15] = [0, 'a', True]
+    world.map[31][1] = [0, '=', True]
+    world.map[32][1] = [0, 'L', True]
+    world.map[33][1] = [0, 'a', True]
+    world.map[34][1] = [0, 'v', True]
+    world.map[35][1] = [0, 'a', True]
    
     world.map[12][15] = world.WORLD_LAVA
     world.objects.append(world_object.world_object(invis_dmg.update, invis_dmg.fire_collide, invis_dmg.char, invis_dmg.color, invis_dmg.type, 12, 15, {}))
@@ -28,4 +36,3 @@ def generate():
     world.objects.append(world_object.world_object(invis_dmg.update, invis_dmg.fire_collide, invis_dmg.char, invis_dmg.color, invis_dmg.type, 6, 18, {}))
     world.map[3][9] = world.WORLD_LAVA
     world.objects.append(world_object.world_object(invis_dmg.update, invis_dmg.fire_collide, invis_dmg.char, invis_dmg.color, invis_dmg.type, 3, 9, {}))
-
