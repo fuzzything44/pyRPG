4def68358484dc05774d71295705eb202a0e0498
CoryReck98 <cory.reck98@gmail.com>
2016-07-23 10:02:11 -0400

    Fixed regen to not go over max health and scale with magic

--- pyRPG/effects/regen.py
+++ pyRPG/effects/regen.py
@@ -1,17 +1,23 @@
 def regen1(obj, delta_time):
     try:
-        obj.attributes["HP"] += 1.0 * delta_time / 1000
+        obj.attributes["HP"] += 1.0 * .5 * obj.attributes["magic"] * delta_time / 1000
+        if obj.attributes["HP"] > obj.attributes["maxHP"]:
+            obj.attributes["HP"] = obj.attributes["maxHP"]
     except:
         pass
 
 def regen2(obj, delta_time):
     try:
-        obj.attributes["HP"] += 2.0 * delta_time / 1000
+        obj.attributes["HP"] += 2.0 * .5 * obj.attributes["magic"] * delta_time / 1000
+        if obj.attributes["HP"] > obj.attributes["maxHP"]:
+            obj.attributes["HP"] = obj.attributes["maxHP"]
     except:
         pass
 
 def regen3(obj, delta_time):
     try:
-        obj.attributes["HP"] += 3.0 * delta_time / 1000
+        obj.attributes["HP"] += 3.0 * .5 * obj.attributes["magic"] * delta_time / 1000
+        if obj.attributes["HP"] > obj.attributes["maxHP"]:
+            obj.attributes["HP"] = obj.attributes["maxHP"]
     except:
         pass
