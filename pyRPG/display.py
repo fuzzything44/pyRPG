33f88eb758bb52bc18de1f72bc706fba6af92ce5
fuzzything44 <slimesftw@gmail.com>
2016-09-01 14:41:36 -0600

    Work on menu improvement.
    Should have color and multiline options now.

--- pyRPG/display.py
+++ pyRPG/display.py
@@ -2,6 +2,8 @@ from unicurses import *
 from win32 import win32api
 import win32con as CONST
 
+import display
+
 def keyDown(key):
     return (win32api.GetAsyncKeyState(key) & (1 << 15)) >> 15
 
@@ -70,12 +72,16 @@ fn_params: A list of lists. fn_params[n] will be the list of arguments to be pas
 
 Returns an int corresponding to the option chosen. Min of 0, max of (Num_options - 1)
 """
-    substrs = text.split('\n')
-    substrs.append('')
-    line = 5
+
+    # Intro text stuff.
+    substrs = text.split('\n') # Split their intro text on lines.
+    substrs.append('') # Add spacing line after intro text.
+    line = 5 # Current line to print on
     while keyDown(ord('E')) or keyDown(CONST.VK_RETURN):
         pass
     flushinp()
+    if len(substrs) > 10:
+        raise Exception("Menu Error: Too long of basic description. Be more concise.")
     for str in substrs:
         # Cut off strings if too long.
         str = str[:29]
@@ -83,31 +89,86 @@ Returns an int corresponding to the option chosen. Min of 0, max of (Num_options
         str = str + ' ' * (29 - len(str))
         printc(50,  line, str)
         line += 1
-        if line > 24: # Overflowed screen
-            key = getch()
-            while key == -1: # Wait for keypress
-                key = getch()
-            for i in range(20): # Clear menu
-                printc(50, i + 5, ' ' * 29)
-            line = 5 # Reset line
-    # Before displaying options, check if all can fit.
-    if len(opt_list) > (24 - line): # overflowed screen
-        key = getch()
-        while key == -1: # Wait for keypress
-            key = getch()
-        for i in range(20): # Clear menu
-            printc(50, i + 5, ' ' * 29)
-        line = 5 # Reset line
-    if len(opt_list) > 20: # Too long for one menu
-        raise Exception("Menu Error: Too many options. Try to lower the amount.")
     
-    # Finally can make menu
+    # Format their options to be better.
+    # Option class: can print itself, knows how many lines it is
+    class option:
+        def __init__(this, str, func): # str is the given string.
+            this.function = func
+            this.disps = [] # disps is all strings it will display
+            curr_str = ""
+            curr_index = 0 # How far we are along the screen. Can go to 28.
+            curr_color = display.WHITE
+            curr_line = 0
+            
+            setting_color = False
+            for chr in str:
+                if setting_color:
+                    setting_color = False
+                    # Add the string. curr_index - len(curr_string) to get the start of the string, not the end.
+                    this.disps.append([curr_index - len(curr_str), curr_line, curr_str, curr_color])
+                    curr_str = "" # Reset string
+                    curr_color = this.get_color(chr) # Set color to what they chose.
+                elif chr == '\\':
+                    setting_color = True
+                else:
+                    if chr != '\n':
+                        curr_str += chr # Add the character to the string.
+                    curr_index += 1
+                    if (curr_index == 28) or (chr == '\n'): # Max size or they wanted a new line
+                        this.disps.append([curr_index - len(curr_str), curr_line, curr_str, curr_color])
+                        curr_index = 1 # Add small indent
+                        curr_str = ""
+                        curr_line += 1
+            this.disps.append([curr_index - len(curr_str), curr_line, curr_str, curr_color])
+            this.lines = curr_line + 1 # Amount of lines it takes up.
+            if this.lines > 7:
+                raise Exception("Menu Error: Wayyy too long of an option. Really, over 7 lines?!")
+        def disp(this, start_line):
+            for elem in this.disps:
+                # Elements have 4 variables: x location, line, text, color
+                # Elements have to be shifted 51 on X-axis and start_line on Y-axis.
+                display.printc(elem[0] + 51, elem[1] + start_line, elem[2], elem[3]) 
+
+        def get_color(this, chr): # Gets the color corresponding to the given character
+              if chr == 'r':
+                 return display.RED
+              if chr == 'g':
+                  return display.GREEN
+              if chr == 'b':
+                  return display.BLUE
+              if chr == 'c':
+                  return display.CYAN
+              if chr == 'm':
+                  return display.MAGENTA
+              if chr == 'y':
+                  return display.YELLOW
+              return display.WHITE # Just return white if unknown (or w)
+        
+
+    # Turn strings in opt_list to options
+    PAGE_SIZE = 24 - line # Max options in a page
+    pages = [[]] # List of pages, contains list of options.
+    linecount = 0 # How many lines all our options take up.
+    for opt in opt_list: 
+        optn = option(opt[0], opt[1]) # Create option from their string
+        if linecount + optn.lines >= PAGE_SIZE: # If overflow of page
+            pages.append([]) # New page
+            linecount = 0 # Haven't used any of it.
+        pages[-1].append(optn) # Add option
+        
+    curr_page = 0
+    choice = 0
     menu_min = line # Can't go farther up than this.
-    for opt in opt_list:
-        # The [:28] is to slice the string as to not overflow the menu
-        printc(51, line, opt[0][:28])
-        line += 1
-    menu_max = line # Can't get to this or above
+
+    # Display first page
+    # Start by clearing area.
+    for index in range(5, 24):
+        display.printc(50, index, ' ' * 28)
+    for optn in pages[curr_page]: # Display everything
+        optn.disp(line)
+        line += optn.lines
+
     # Draw cursor
     printc(50, menu_min, '>')
     cursor_loc = menu_min
@@ -116,9 +177,10 @@ Returns an int corresponding to the option chosen. Min of 0, max of (Num_options
     while True:
         refresh()
         if (keyDown(ord('W')) or keyDown(CONST.VK_UP)) and can_W:
-            if cursor_loc > menu_min:
+            if choice > 0:
                 printc(50, cursor_loc, ' ')
-                cursor_loc -= 1
+                choice -= 1
+                cursor_loc -= pages[curr_page][choice].lines
                 printc(50, cursor_loc, '>')
             can_W = False
         if not (keyDown(ord('W')) or keyDown(CONST.VK_UP)):
@@ -126,9 +188,9 @@ Returns an int corresponding to the option chosen. Min of 0, max of (Num_options
 
         if (keyDown(ord('S')) or keyDown(CONST.VK_DOWN)) and can_S:
             printc(50, cursor_loc, ' ')
-            cursor_loc += 1
-            if cursor_loc == menu_max:  # Can't get to max...
-                cursor_loc -= 1
+            if choice < len(pages[curr_page]) - 1:
+                cursor_loc += pages[curr_page][choice].lines
+                choice += 1
             printc(50, cursor_loc, '>')
             can_S = False
         if not (keyDown(ord('S')) or keyDown(CONST.VK_DOWN)):
@@ -138,7 +200,7 @@ Returns an int corresponding to the option chosen. Min of 0, max of (Num_options
             for i in range(20): # Clear menu
                 printc(50, i + 5, ' ' * 29)
             # Call function
-            opt_list[cursor_loc - menu_min][1](*fn_params[cursor_loc - menu_min])
+            pages[curr_page][choice].function(*fn_params[choice])
             while (keyDown(ord('E')) or keyDown(CONST.VK_RETURN)):
                 pass
             return cursor_loc - menu_min
